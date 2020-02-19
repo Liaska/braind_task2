70761fd2e92df679f618cc606089826d07310928
Lyaska <kostyalyaska@yandex.ru>
2020-02-19 21:06:52 +0300

    add project

---
+++ src/_scripts/main.js
@@ -0,0 +1,32 @@
+(function() {
+	function addToCartHanlder() {
+		let btns  = $('[data-add-to-cart]');
+
+		btns.on('click', function(e){
+			e.preventDefault();
+			
+			$.ajax({
+				url: './fakedata/cartResponse.json',
+				type: 'GET',
+				dataType: 'json',
+				success: (res) => {
+					if (res["success"]) {
+						$(this)
+							.text('Добавлено')
+							.removeClass('btn-primary')
+							.addClass('btn-success');
+						
+						
+						$('[data-cart-count]').text(res['count']);
+				}
+			}
+			});
+		});
+	};
+
+	function main() {
+		addToCartHanlder();
+	};
+
+	main();
+}());
